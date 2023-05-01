4a9ef42c2058171d0587744baca55c7a58bb63b6
ImranPatel7 <imranpatel0702@gmail.com>
2023-05-01 10:55:52 +0530

    working on search functionality

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,4 +1,15 @@
-import { Button, Container, HStack, Radio, RadioGroup } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  HStack,
+  Radio,
+  RadioGroup,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Flex,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { server } from "../index";
@@ -12,6 +23,7 @@ const Coins = () => {
   const [error, setError] = useState(false);
   const [page, setPage] = useState(1);
   const [currency, setCurrency] = useState("inr");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const currencySymbol =
     currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
@@ -22,6 +34,12 @@ const Coins = () => {
   };
 
   const btns = new Array(100).fill(1);
+
+  const filteredCoins = coins.filter(
+    (coin) =>
+      coin.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      coin.symbol.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   useEffect(() => {
     const fetchCoins = async () => {
@@ -49,29 +67,76 @@ const Coins = () => {
         <Loader />
       ) : (
         <>
-          <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
-            <HStack
-              justifyContent={["center", "space-evenly"]}
-              spacing={"4"}
-              p={"8"}
-              bgColor={"blackAlpha.800"}
-              color={"white"}
-              borderRadius="xl"
-            >
-              <Radio size="lg" value={"inr"}>
-                INR(₹)
-              </Radio>
-              <Radio size="lg" value={"usd"}>
-                USD($)
-              </Radio>
-              <Radio size="lg" value={"eur"}>
-                EURO(€)
-              </Radio>
-            </HStack>
-          </RadioGroup>
+          {/* <input
+            type="text"
+            placeholder="Search coins..."
+            value={searchQuery}
+            onChange={(event) => setSearchQuery(event.target.value)}
+          /> */}
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+            minHeight="10vh"
+          >
+            <InputGroup size="md" w={{ base: "80%", md: "60%" }} my={8}>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<SearchIcon color="gray.300" />}
+              />
+              <Input
+                type="text"
+                placeholder="Search coins..."
+                value={searchQuery}
+                onChange={(event) => setSearchQuery(event.target.value)}
+                focusBorderColor="teal.500"
+                borderColor="gray.500"
+                borderRadius="xl"
+              />
+            </InputGroup>
+          </Flex>
+
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+            minHeight="10vh"
+          >
+            <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+              <HStack
+                justifyContent={["center", "space-evenly"]}
+                spacing={"4"}
+                p={"8"}
+                bgColor={"blackAlpha.800"}
+                color={"white"}
+                borderRadius="xl"
+              >
+                <Radio size="lg" value={"inr"}>
+                  INR(₹)
+                </Radio>
+                <Radio size="lg" value={"usd"}>
+                  USD($)
+                </Radio>
+                <Radio size="lg" value={"eur"}>
+                  EURO(€)
+                </Radio>
+              </HStack>
+            </RadioGroup>
+          </Flex>
 
           <HStack wrap={"wrap"} justifyContent={["center", "space-evenly"]}>
-            {coins.map((i) => (
+            {/* {coins.map((i) => (
+              <CoinCard
+                id={i.id}
+                key={i.id}
+                name={i.name}
+                price={i.current_price}
+                img={i.image}
+                symbol={i.symbol}
+                currencySymbol={currencySymbol}
+              />
+            ))} */}
+            {filteredCoins.map((i) => (
               <CoinCard
                 id={i.id}
                 key={i.id}
